bdd8069149005255f74b7f0721728fa8ec271e3b
waleedtaie <61197947+waleedtaie@users.noreply.github.com>
2020-03-16 20:52:34 +0200

    env

--- GUVM_agent.sv
+++ GUVM_agent.sv
@@ -1,8 +1,8 @@
 class processor_agent extends uvm_agent;
   `uvm_component_utils(GUVM_agent)
     
-    processor_driver driver;
-    uvm_sequencer#(processor_transaction) sequencer;
+    GUVM_driver driver;
+    uvm_sequencer#(sequence_item) sequencer;
     
     function new(string name, uvm_component parent);
       super.new(name, parent);
@@ -10,7 +10,7 @@ class processor_agent extends uvm_agent;
     
     function void build_phase(uvm_phase phase);
       driver = GUVM_driver::type_id::create("driver", this);
-      sequencer = uvm_sequencer#(processor_transaction)::type_id::create("sequencer", this);
+      sequencer = uvm_sequencer#(sequence_item)::type_id::create("sequencer", this);
     endfunction    
     
     // In UVM connect phase, we connect the sequencer to the driver.
