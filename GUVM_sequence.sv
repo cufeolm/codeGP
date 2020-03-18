9a5a9de3a5cad633caf0466c47105bbdf10550e7
mostafa552 <mostafa_552@yahoo.com>
2020-03-18 21:33:36 +0200

    Update GUVM_sequence.sv

--- GUVM_sequence.sv
+++ GUVM_sequence.sv
@@ -1,20 +1,20 @@
-class GUVM_sequence extends uvm_sequence #(cmd_sequence_item);
+class GUVM_sequence extends uvm_sequence #(GUVM_sequence_item);
    
    `uvm_object_utils(generic_sequence);
-   cmd_sequence_item command ;
+   GUVM_sequence_item command ;
 
    function new(string name = "GUVM_sequence");
       super.new(name);
    endfunction : new
  
    task body();
-      command = cmd_sequence_item::type_id::create("command");
+      command = GUVM_sequence_item::type_id::create("command");
       //command.inst = 1;
       repeat(10) begin
          start_item(command);
          command.ran();
          finish_item(command);
-         command = cmd_sequence_item::type_id::create("command");
+         command = GUVM_sequence_item::type_id::create("command");
       end
    endtask : body
 
