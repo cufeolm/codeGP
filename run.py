7e285dc45c1019c4febee96484ae4841c443aa78
waleedtaie <61197947+waleedtaie@users.noreply.github.com>
2020-04-07 20:34:28 +0200

    ..

--- run.py
+++ run.py
@@ -1,27 +1,22 @@
 import os
 
-s="""
-welcome to GUVM user interface
-please choose which dut to compile :
-1-riscy:based on RISC-v ISA 
-2-leon :based on Sparcv8 ISA 
-3-amber:based on ARM ISA 
+s = """
+[[[ welcome to GUVM user interface ]]]
+please choose which dut to compile:
+1- Riscy core (based on RISC-v ISA): enter --> 1 
+2- Leon core: (based on Sparcv8 ISA): enter --> 2
+3- Amber core: (based on ARM ISA): enter --> 3
 DUT: """;
 
-
-
-
-
-
-g = raw_input(s) ;
-print g ;
+g = raw_input(s);
+print g;
 if g == "1":
-  os.system("vsim -c -do riscy/run_riscy.do" )
+  os.system("vsim -c -do riscy/run_riscy.do")
 elif g == "2":
   os.system("vsim -c -do leon/run_leon.do")
 elif g == "3":
   os.system("vsim -c -do amber/run_amber.do")
-else :
+else:
   print("please enter a valid number")
-  
+
 input('Press any key to exit')
