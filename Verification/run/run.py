eba5e561fb035e3dec7b52d71cd61dc266bc9a83
mostafa552 <mostafa_552@yahoo.com>
2020-04-12 00:18:55 +0200

    modified:   Verification/run/run.py

--- Verification/run/run.py
+++ Verification/run/run.py
@@ -6,17 +6,21 @@ please choose which dut to compile:
 1- Riscy core (based on RISC-v ISA): enter --> 1 
 2- Leon core (based on Sparcv8 ISA): enter --> 2
 3- Amber core (based on ARM ISA): enter --> 3
+any other input wil terminate the simulation
 DUT: """;
-
-g = raw_input(s);
-print g;
-if g == "1":
-  os.system("vsim -c -do ../testing_riscy/run_riscy.do")
-elif g == "2":
-  os.system("vsim -c -do ../testing_leon/run_leon.do")
-elif g == "3":
-  os.system("vsim -c -do ../testing_amber/run_amber.do")
-else:
-  print("please enter a valid number")
-
-input('Press any key to exit')
+while (1):
+	
+	g = raw_input(s);
+	print g;
+	if g == "1":
+		os.system("vsim -c -do ../testing_riscy/run_riscy.do")
+	elif g == "2":
+		os.system("vsim -c -do ../testing_leon/run_leon.do")
+	elif g == "3":
+		os.system("vsim -c -do ../testing_amber/run_amber.do")
+	else:
+		print("please enter a valid number")
+		break;
+		
+	raw_input('Press any key to start again')
+	os.system("cls")
